cd1802e1ec70de33d4802cc07fb068d3497638b8
Sripath Cherukuri <sripath@Sripaths-MBP.eduroam.gmu.edu>
2023-04-24 17:00:47 -0400

    added create room page

--- frontend/app/routes/room/index.jsx
+++ frontend/app/routes/room/index.jsx
@@ -1,9 +1,20 @@
+import styled from "styled-components"
+import createRoomStyles from "../../styles/index.room.css"
 const Room = () => {
     return (
-        <div>
-
+        <div className="container">                   
+            <h4 className="create-room-heading">Welcome to Codenames</h4>
+            <label className="label-style">To enter into the session, choose a nickname.</label>
+            <br></br><br></br>
+            <input className="text-container" type="text" placeholder="Enter your nickname"/>
+            <br></br><br></br>
+            <button className="button-style">Create Room</button>
         </div>
     )
+}
+
+export function links() {
+    return [{ rel: 'stylesheet', href: createRoomStyles }]
 }
 
 export default Room;
