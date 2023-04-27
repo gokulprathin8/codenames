3bf33f7816f034d1c158622654531df48ec6ad36
Sripath Cherukuri <sripath.cherukuri@gmail.com>
2023-04-27 19:52:46 -0400

    made changes to playgame page

--- frontend/app/routes/playgame.jsx
+++ frontend/app/routes/playgame.jsx
@@ -1,6 +1,6 @@
 import playGameStyles from "../styles/playgame.room.css";
-import playerOneImage from "../../public/images/player_one.svg";
-import playerTwoImage from "../../public/images/player_two.svg";
+import playerOneImage from "../../public/images/spyware.png";
+import playerTwoImage from "../../public/images/spyware.png";
 
 const playGame = () => {
   return (
@@ -9,7 +9,8 @@ const playGame = () => {
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         </head>
       <div class="boxes-container">
-          <div className="container-red">
+        <div className="leftmost-container">
+        <div className="container-red">
             <div>
               <img
                 className="player-icon"
@@ -28,36 +29,36 @@ const playGame = () => {
             <br></br>
             <button className="red-button">Join as Spymaster</button>
           </div>
+        </div>
 
           <div class="cards-container-5-by-5">
-            <div class="card">1a</div>
-            <div class="card">2a</div>
-            <div class="card">3a</div>
-            <div class="card">4a</div>
-            <div class="card">5a</div>
-            <div class="card">1b</div>
-            <div class="card">2b</div>
-            <div class="card">3b</div>
-            <div class="card">4b</div>
-            <div class="card">5b</div>
-            <div class="card">1c</div>
-            <div class="card">2c</div>
-            <div class="card">3c</div>
-            <div class="card">4c</div>
-            <div class="card">5c</div>
-            <div class="card">1d</div>
-            <div class="card">2d</div>
-            <div class="card">3d</div>
-            <div class="card">4d</div>
-            <div class="card">5d</div>
-            <div class="card">1e</div>
-            <div class="card">2e</div>
-            <div class="card">3e</div>
-            <div class="card">4e</div>
-            <div class="card">5e</div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
           </div>
-
-          <div className="combined-two-boxes">
+          <div className="rightmost-container">
           <div className="container-blue">
             <div>
               <img
@@ -78,10 +79,9 @@ const playGame = () => {
             <button className="blue-button">Join as Spymaster</button>
           </div>
           <div class="bottom-box">
-            playGame Log
-            <div>
+            <p className="chat-tittle">Game Log</p>
               <input id="textclue" type="text" placeholder="Enter the clue word"></input>
-            </div>
+              <input id="submitid" type="submit" value="Send"></input>
           </div>
           </div>
       </div>
