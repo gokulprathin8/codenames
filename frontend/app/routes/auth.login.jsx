82920eda53b5bc8ce3626bba9b05a4a226c80001
Gokul Prathin <gokulprathin8@gmail.com>
2023-04-27 18:45:35 -0400

    added logins styles

--- frontend/app/routes/auth.login.jsx
+++ frontend/app/routes/auth.login.jsx
@@ -9,6 +9,25 @@ const LoginPageImage = styled.img`
     height: auto;
 `;
 
+const CredentialContainer = styled.div`
+  margin-top: 40px;
+`;
+
+const UserEmailInput = styled.input`
+  padding: 10px;
+  border: 1px solid grey;
+  border-radius: 10px;
+  margin-top: 5px;
+
+  &:hover {
+    border: 1px solid var("--g-green");
+  }
+
+  &:focus {
+    border-radius: 1px solid var("--g-green");
+  }
+`;
+
 export default function AuthLogin() {
     return (
         <LoginContainer>
@@ -17,11 +36,21 @@ export default function AuthLogin() {
             </div>
 
             <div style={{ flex: 1 }}>
-                <div style={{ padding: '50px', fontFamily: "Roboto" }}>
+                <div style={{ padding: "calc(30% - 100px)", fontFamily: "Roboto" }}>
                     <h1 style={{ marginBottom: "5px" }}>Get's Started.</h1>
                     <p style={{ color: "grey", marginTop: 0 }}>Already have a account?
-                        <a href="#" style={{ textDecoration: "none", color: "#54927A" }}> Login</a>
+                        <a href="#" style={{ textDecoration: "none", color: "var(--g-green)" }}> Login</a>
                     </p>
+
+                    <CredentialContainer>
+                        <p style={{ color: "var(--g-silver)" }}>Email address</p>
+                        <UserEmailInput
+                            type="email"
+                            id="user-email"
+                            required={true}
+                            minLength={5}
+                        />
+                    </CredentialContainer>
                 </div>
             </div>
         </LoginContainer>
