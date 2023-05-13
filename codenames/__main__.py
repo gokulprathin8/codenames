3ce26170720de5a20c961cf41d3367ada959bffc
Gokul Prathin A <gokulprathin8@gmail.com>
2023-05-13 00:07:43 -0400

    support https - SSL Certificate added

--- codenames/__main__.py
+++ codenames/__main__.py
@@ -1,3 +1,5 @@
+import ssl
+
 import uvicorn
 
 from codenames.settings import settings
@@ -5,6 +7,12 @@ from codenames.settings import settings
 
 def main() -> None:
     """Entrypoint of the application."""
+
+    # configure ssl for https
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.load_cert_chain(certfile="codenames/ssl-certificate/cert.pem",
+                                keyfile="codenames/ssl-certificate/key.pem")
+
     uvicorn.run(
         "codenames.web.application:get_app",
         workers=settings.workers_count,
@@ -13,6 +21,8 @@ def main() -> None:
         reload=settings.reload,
         log_level=settings.log_level.value.lower(),
         factory=True,
+        ssl_keyfile="codenames/ssl-certificate/key.pem",
+        ssl_certfile="codenames/ssl-certificate/cert.pem"
     )
 
 
--- frontend/app/constants.js
+++ frontend/app/constants.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = "http://localhost:8000/api/";
+export const SERVER_URL = "https://0.0.0.0:8000/api/";
 export const DEV = true;
