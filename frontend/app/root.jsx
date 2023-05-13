7f2920cb03f4e3679683d2c3258c68f166812989
Gokul Prathin A <gokulprathin8@gmail.com>
2023-05-13 01:01:53 -0400

    https remix

--- frontend/app/root.jsx
+++ frontend/app/root.jsx
@@ -8,6 +8,8 @@ import {
 } from "@remix-run/react";
 import 'semantic-ui-css/semantic.min.css';
 import globalStyles from "./styles/global.css";
+import {red} from "@remix-run/dev/dist/colors";
+import {redirect} from "@remix-run/node";
 
 export default function App() {
   return (
@@ -31,4 +33,22 @@ export default function App() {
 
 export function links() {
     return [{ rel: 'stylesheet', href: globalStyles}]
+}
+
+export function loader({ request }) {
+    let url = new URL(request.url);
+    let hostname = url.hostname;
+    let proto = request.headers.get("X-Forwarded-Proto") ?? url.protocol;
+
+    url.host = request.headers.get("X-Forwarded-Host") ?? request.headers.get("host") ?? url.host;
+    url.protocol = "https:";
+
+    if (proto === "http") {
+        return redirect(url.toString(), {
+            headers: {
+                "X-Forwarded-Proto": "https",
+            },
+        })
+    }
+    return {};
 }
